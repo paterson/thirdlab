86c5bf22d063d3a004751d92b1cb40b33c9c19aa
Niall Paterson <npaters@tcd.ie>
2016-11-19 21:54:36 +0000

    Update chatroommanager.go

--- chatroom/chatroommanager.go
+++ chatroom/chatroommanager.go
@@ -47,7 +47,7 @@ func (manager ChatroomManager) pollClient(client Client) {
 			fmt.Println("Received:", strings.TrimSpace(input))
 			manager.input <- Input{Text: strings.TrimSpace(input), Client: client}
 		}
-		connected = err == nil // Check if client is disconnected
+		//connected = err == nil // Check if client is disconnected
 	}
 }
 
