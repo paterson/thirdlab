7c63fe211b455b6e19ca6d5b984c85ab076e669c
Niall Paterson <npaters@tcd.ie>
2016-11-19 23:36:21 +0000

    Bug fix

--- chatroom/member.go
+++ chatroom/member.go
@@ -45,7 +45,7 @@ func (member Member) SendLeaveMessage() {
 	member.Client.Connection.Write([]byte(str))
 }
 
-func (member Member) SendErrorMessage(code int, message string) {
+func (member Member) SendErrorMessage(code string, message string) {
 	lines := []string{
 		"ERROR_CODE:" + code,
 		"ERROR_DESCRIPTION:" + message,
